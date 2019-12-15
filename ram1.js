f7ae108ab665ee7b6a3c5dbd10ecb07e74867ff8
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2019-12-14 19:56:39 -0800

    Update ram1.js

--- ram1.js
+++ ram1.js
@@ -1,7 +1,9 @@
 javascript:
 
 var world_url = "https://en110.tribalwars.net";
-var screen = window.location.href.match(/screen=([a-z]+)/)[1];
+var screen = window.location.href.match(/screen=([a-z_]+)/)[1];
+
+var wall_rams = [2,4,5,10,14,19];
 
 function attack_village() {
   var defender_raw_village = document.getElementById('attack_info_def').rows[1].cells[1].firstChild.firstChild;
@@ -12,13 +14,31 @@ function attack_village() {
 }
 
 function fill_units() {
+  var xy = window.location.href.match(/xy=([|\d]+)/)[1];
+  var wall = window.location.href.match(/wall=([\d]+)/)[1];
+  if (wall > wall_rams.length - 1) wall = wall_rams.length - 1;
+  document.forms[0].input.value = xy;
   document.forms[0].spy.value = 1;
-  document.forms[0].ram.value = 4;
+  document.forms[0].ram.value = wall_rams[wall];
   document.forms[0].axe.value = 50;
 }
 
-if (screen == "report") {
+
+
+if (screen == "am_farm") {
+  var village_list = window.top.$("#plunder_list tr:not(:first-child):visible");
+  console.log("length ="+village_list.length);
+  for (i = 1; i < village_list.length; i++) {
+    var tds = village_list[i].getElementsByTagName("td");
+    var xy = tds[3].innerHTML.split("(")[1].split(")")[0];
+    var wall_level = tds[6].innerHTML;
+    if (wall_level > 0)
+      window.location.href = world_url + "/game.php?screen=place&xy=" + xy + "&wall=" + wall_level;
+  }
+} else if (screen == "report") {
   attack_village();
-} else {
+} else if (screen == "place") {
   fill_units();
+} else {
+  console.log("No command for " + screen);
 }
