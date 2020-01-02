fc908e012057c6b4510f04319134f55454439cf3
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-02 13:24:19 -0800

    Create combined_times.js

---
+++ combined_times.js
@@ -0,0 +1,20 @@
+javascript:
+
+function extractStuff(content) {
+  var raw = content.getElementsByTagName("img")[0].title.split(" - ")[0];
+  return raw;
+}
+
+function doStuff() {
+  var village_list = $("#combined_table tr:not(:first-child)");
+  
+  for (i = 0; i < village_list.length; i++) {
+    var tds = village_list[i].getElementsByTagName("td");
+    var building = extractStuff(tds[2]);
+    var rax = extractStuff(tds[3]);
+    var stable = extractStuff(tds[4]);
+    console.log(building + "\t" + rax + "\t" + stable);
+  }
+}
+  
+doStuff();
