808b89c8aaf0b37f5dff255b9225c7bdd3a74b90
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-05 09:24:15 -0800

    Update combined_times.js

--- combined_times.js
+++ combined_times.js
@@ -1,15 +1,46 @@
 javascript:
 
-var now = new Date($("#serverDate")[0].innerText + " " + $("#serverTime")[0].innerText);
+var now_time = $("#serverTime")[0].innerText;
+var now_date_raw = $("#serverDate")[0].innerText;
+var now_date_matched = now_date_raw.match(/(\d+)\/(\d+)\/(\d+)/);
+var now = new Date(now_date_matched[2] + "/" + now_date_matched[1] + "/" + now_date_matched[3] + " " + now_time);
 var tomorrow = new Date(now); 
 tomorrow.setDate(tomorrow.getDate() + 1);
+console.log(now);
+console.log(tomorrow);
 
 function extractStuff(content) {
   var raw = content.getElementsByTagName("img")[0].title.split(" - ")[0];
   raw = raw.replace("on ", "");
-//  raw = raw.replace("today", now.getDay() + "." + now.getMonth() + ".");
-//  raw = raw.replace("tomorrow", tomorrow.getDay() + "." + tomorrow.getMonth() + ".");
-  return raw;
+  var day = 0;
+  var month = 0;
+  if (raw.includes("No recruitment")) {
+      return now;
+  } else if (raw.includes("No production")) {
+      return now;
+  } else if (raw.includes("today")) {
+    day = now.getDate();
+    month = now.getMonth();
+  } else if (raw.includes("tomorrow")) {
+    day = tomorrow.getDate();
+    month = tomorrow.getMonth();
+  } else {
+    var daymonth = raw.match(/(\d+)\.(\d+)\./);
+    day = parseInt(daymonth[1]);
+    month = parseInt(daymonth[2]) - 1;
+  }
+  
+  var hourminute = raw.match(/(\d+):(\d+)/);
+  var hour = hourminute[1];
+  var minute = hourminute[2];
+  
+  var result = new Date(now);
+  result.setMonth(month);
+  result.setDate(day);
+  result.setHours(hour);
+  result.setMinutes(minute);
+  
+  return result;
 }
 
 function doStuff() {
@@ -20,8 +51,9 @@ function doStuff() {
     var building = extractStuff(tds[2]);
     var rax = extractStuff(tds[3]);
     var stable = extractStuff(tds[4]);
-    document.body.innerHTML += building + "</br>" + rax + "</br>" + stable + "</p>";
+    if (building < tomorrow || rax < tomorrow || stable < tomorrow) 
+      document.body.innerHTML += tds[1].innerText.trim() + "</br>" + building + "</br>" + rax + "</br>" + stable + "</p>";
   }
 }
-  
+
 doStuff();
