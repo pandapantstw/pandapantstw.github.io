c77006bb1f4d3d9113522865a8767ae4d76b4aee
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-04 19:46:10 -0800

    Update combined_times.js

--- combined_times.js
+++ combined_times.js
@@ -1,7 +1,14 @@
 javascript:
 
+var now = new Date($("#serverDate")[0].innerText + " " + $("#serverTime")[0].innerText);
+var tomorrow = new Date(now); 
+tomorrow.setDate(tomorrow.getDate() + 1);
+
 function extractStuff(content) {
   var raw = content.getElementsByTagName("img")[0].title.split(" - ")[0];
+  raw = raw.replace("on ", "");
+//  raw = raw.replace("today", now.getDay() + "." + now.getMonth() + ".");
+//  raw = raw.replace("tomorrow", tomorrow.getDay() + "." + tomorrow.getMonth() + ".");
   return raw;
 }
 
