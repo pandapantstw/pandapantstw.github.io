31327bfd6b4f2f3e0bbe20e5aee891dcd6d10bfe
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-06 19:57:19 -0800

    Update recruit.js

--- recruit.js
+++ recruit.js
@@ -6,8 +6,16 @@ var scout_index = 4;
 
 var hours = 12;
 var refill_factor = 2;
+
 var off_per_hour = [0,0,20,0,0,10,0,0,1,0,0];
 var off_limit = [0,0,6000,0,0,3000,0,0,240,0,0];
+
+var def_per_hour = [20,2.5,0,0,0,0,0,5,0,0,0];
+var def_limit = [7500,1000,0,0,0,0,0,2000,0,0,0];
+
+var scout_per_hour = [14,7.5,0,0,14,0,0,1.4,0,0,0];
+var scout_limit = [5000,2600,0,0,5000,0,0,500,0,0,0];
+
 var unit_cost = [
   [50,30,10],
   [30,30,70],
@@ -49,9 +57,22 @@ function doStuff() {
       built = built.concat(parseInt(tds[j].getElementsByTagName("input")[0].getAttribute("data-existing")));
       building = building.concat(parseInt(tds[j].getElementsByTagName("input")[0].getAttribute("data-running")));
     }
-    // Skip defensive
-    if (built[spear_index] + building[spear_index] > 100) continue;
-    var target = off_per_hour.slice();
+    
+    var limit;
+    var target;
+    if (built[axe_index] + building[axe_index] > 100) {
+      limit = off_limit;
+      target = off_per_hour.slice();
+    } else if (built[scout_index] + building[scout_index] > 200) {
+      limit = scout_limit;
+      target = scout_per_hour.slice();
+    } else if (built[spear_index] + building[spear_index] > 100) {
+      limit = def_limit;
+      target = def_per_hour.slice();
+    } else {
+      continue;
+    }
+    
     var resource_cost = [0,0,0];
     var should_build = false;
     // Loop around the list of units to calculate production targets and costs
@@ -61,12 +82,10 @@ function doStuff() {
       target[j] *= refill_factor;
       target[j] -=  building[j];
       if (target[j] < 0) target[j] = 0;
-      if (target[j] + built[j] > off_limit[j]) target[j] = off_limit[j] - built[j];
+      if (target[j] + built[j] > limit[j]) target[j] = limit[j] - built[j];
       if (target[j] < 0) target[j] = 0;
       var resource_cost = vector_add(resource_cost, vector_mul(unit_cost[j], target[j]));
     }
-    console.log(resource_cost);
-    console.log(resources);
     if (!should_build) continue;
     console.log("Want to fill " + i + " with [" + target + "]");
     var ratio = 1;
@@ -76,6 +95,7 @@ function doStuff() {
         should_build = false;
       }
     }
+    if (ratio < 0.1) continue;
     console.log("Filling " + i + " with [" + target + "] at ratio " + ratio);
     for (var j = 0 ; j < target.length; j++) {
       tds[3 + j].getElementsByTagName("input")[0].value = parseInt(ratio * target[j]);
