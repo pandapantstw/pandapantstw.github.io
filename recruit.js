de0bf9077e02c4c63027fea271cbc40575f192b3
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-06 18:39:10 -0800

    Update recruit.js

--- recruit.js
+++ recruit.js
@@ -1,15 +1,47 @@
 javascript:
 
-var axe_index;
-var spear_index;
-var scout_index;
+var spear_index = 0;
+var axe_index = 2;
+var scout_index = 4;
+
+var hours = 5
+var refill_factor = 2;
+var off_per_hour = [0,0,20,0,0,10,0,0,1,0,0];
+var off_limit = [0,0,6000,0,0,3000,0,0,240,0,0];
+var unit_cost = [
+  [50,30,10],
+  [30,30,70],
+  [60,30,40],
+  [100,30,60],
+  [50,50,20],
+  [125,100,250],
+  [250,100,150],
+  [200,150,600],
+  [300,200,200],
+  [320,400,100],
+  [20,20,40],
+  [40000,50000,50000],
+];
+
+function vector_add(a, b) {
+  var result = a.slice();
+  for (var i = 0; i < result.length; i++) result[i] += b[i];
+  return result;
+}
+function vector_mul(a, b) {
+  var result = a.slice();
+  for (var i = 0; i < result.length; i++) result[i] *= b;
+  return result;
+}
 
 function doStuff() {
   var rows = $("#mass_train_table tr");
   
-  for(var i = 1; i < 2; i++) {
+//  for(var i = 1; i < 2; i++) {
+  for(var i = 1; i < rows.length; i++) {
     var tds = rows[i].getElementsByTagName("td");
     var resources = tds[1].innerText.replace(/\./g, "").split("\n");
+    for (var j = 0 ; j < resources.length; j++) resources[j] = parseInt(resources[j]);
     var farm = tds[2].innerText.split("/");
     var built = [];
     var building = [];
@@ -17,8 +49,32 @@ function doStuff() {
       built = built.concat(parseInt(tds[j].getElementsByTagName("input")[0].getAttribute("data-existing")));
       building = building.concat(parseInt(tds[j].getElementsByTagName("input")[0].getAttribute("data-running")));
     }
-    console.log(built);
-    console.log(building);
+    // Skip defensive
+    if (built[spear_index] + building[spear_index] > 100) continue;
+    var target = off_per_hour.slice();
+    var resource_cost = [0,0,0];
+    var should_build = false;
+    // Loop around the list of units to calculate production targets and costs
+    for (var j = 0 ; j < target.length; j++) {
+      target[j] *= hours;
+      if (building[j] < target[j]) should_build = true;
+      target[j] *= refill_factor;
+      target[j] -=  building[j];
+      if (target[j] < 0) target[j] = 0;
+      if (target[j] + built[j] > off_limit[j]) target[j] = off_limit[j] - built[j];
+      if (target[j] < 0) target[j] = 0;
+      var resource_cost = vector_add(resource_cost, vector_mul(unit_cost[j], target[j]));
+    }
+    if (!should_build) continue;
+    console.log("Want to fill " + i + " with [" + target + "]");
+    for (var j = 0; j < resource_cost; j++) 
+      if (resources[j] < resource_cost[j]) 
+        should_build = false;
+    if (!should_build) continue;
+    console.log("Filling " + i + " with [" + target + "]");
+    for (var j = 0 ; j < target.length; j++) {
+      tds[3 + j].getElementsByTagName("input")[0].value = target[j];
+    }
   }
 }
 doStuff();
