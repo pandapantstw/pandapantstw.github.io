109b33365a9406d2198598f304e63f25e239397d
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-07 19:59:42 -0800

    Update res_counter.js

--- res_counter.js
+++ res_counter.js
@@ -12,6 +12,10 @@ for(var i = 0; i < woods.length; i++) {
   clay += parseInt(clays[i].innerText.replace(".", ""));
   iron += parseInt(irons[i].innerText.replace(".", ""));
 }
-console.log(wood);
-console.log(clay);
-console.log(iron);
+if (wood > 1000) wood = (wood / 1000).toFixed(2) + "k";
+else if (wood > 1000) wood = (wood / 1000000).toFixed(2) + "m";
+if (clay > 1000) clay = (clay / 1000).toFixed(2) + "k";
+else if (clay > 1000) clay = (clay / 1000000).toFixed(2) + "m";
+if (iron > 1000) iron = (iron / 1000).toFixed(2) + "k";
+else if (iron > 1000) iron = (iron / 1000000).toFixed(2) + "m";
+alert("wood = " + wood + "\nclay = " + clay + "\niron = " + iron);
