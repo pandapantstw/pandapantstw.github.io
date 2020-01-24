a5a7da4e83d10913a1605b51af2eb9ad9b6af0ab
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-23 17:35:03 -0800

    Update base.js

--- base.js
+++ base.js
@@ -4,7 +4,7 @@ function parseUrl() {
   var url = new Object();
   url.domain = window.location.href.match(/[a-z]+\d+.tribalwars.net/)[0];
   url.params = new Object();
-  var raw = window.location.href.match(/[0-9a-z_]+=[0-9a-z_]+/g);
+  var raw = window.location.href.match(/[0-9a-z_\|]+=[0-9a-z_\|]+/g);
   for (var i in raw) {
     param = raw[i].split("=");
     url.params[param[0]] = param[1];
