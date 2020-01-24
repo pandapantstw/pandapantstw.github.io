a83d2cf88c91002550e51a7182b016d087bbf8e5
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-23 23:52:51 -0800

    Update nextBarb.js

--- nextBarb.js
+++ nextBarb.js
@@ -1,9 +1,17 @@
 javascript:
 
-var village_xy = parseInt($("#menu_row2_village")[0].parentElement.innerText.match(/\d+\|\d+/)[0].replace("|", ""));
-var villages = [];
-for (v in TWMap.villages) {
-  villages = villages.concat(TWMap.villages[v]);
+function getCookies() {
+  var raw_list = document.cookie.split("; ");
+  var cookies = new Object();
+  for (i = 0; i < raw_list.length; i++) {
+    var raw_split = raw_list[i].split("=");
+    cookies[raw_split[0]] = raw_split[1];
+  }
+  return cookies;
+}
+
+function setCookie(key, value) {
+  document.cookie = key + "=" + value + ";expires=-1";
 }
 
 function distance_sq(a, b) {
@@ -14,13 +22,6 @@ function distance_sq(a, b) {
   return (ax - bx)*(ax - bx) + (ay - by)*(ay - by);
 }
 
-villages.sort(
-  function(a, b) { 
-    a
-    return distance_sq(a.xy, village_xy) - distance_sq(b.xy, village_xy); 
-  }
-);
-
 function nextBarb(villages) {
   for (v in villages) {
     if (villages[v].owner != 0) continue;
@@ -28,4 +29,30 @@ function nextBarb(villages) {
   }
   return undefined;
 }
-console.log(nextBarb(villages));
+
+function gotoNextBarb() {
+  
+}
+
+function main() {
+  var cookies = getCookies();
+  console.log(cookies);
+  var village_xy = parseInt($("#menu_row2_village")[0].parentElement.innerText.match(/\d+\|\d+/)[0].replace("|", ""));
+  var villages = [];
+  for (v in TWMap.villages) {
+    villages = villages.concat(TWMap.villages[v]);
+  }
+
+  villages.sort(
+    function(a, b) {
+      return distance_sq(a.xy, village_xy) - distance_sq(b.xy, village_xy); 
+    }
+  );
+  
+  var target = nextBarb(villages);
+
+  console.log(target);
+}
+
+$.get('https://pandapantstw.github.io/base.js', main);
+void(0);
