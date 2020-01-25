53bf647dc5648062af020754ac5f734512e0d37c
pandapantstw <58375571+pandapantstw@users.noreply.github.com>
2020-01-24 17:19:26 -0800

    Update cat2.js

--- cat2.js
+++ cat2.js
@@ -1,5 +1,9 @@
 javascript:
 
+var num_axe = 20;
+var building_ids = ['main', 'barracks', 'stable', 'garage', 'church', 'church_f', 'snob', 'smith', 'place', 'statue', 'market', 'wood', 'stone', 'iron', 'farm', 'storage', 'hide', 'wall'];
+var building_cats = [2,2,2,3,3,3,3,3,4,4,4,5,5,6,6,6,7,8,8,9,10,10,11,12,13,15,16,17,19,20];
+var building_min = [1,0,0,0,0,0,0,0,1,0,0,30,30,30,1,30,10,0];
 
 function distance_sq(a, b) {
   ax = Math.floor(a / 1000);
@@ -34,14 +38,14 @@ function doMap() {
   goto("screen=info_village&id=" + target.id);
 }
 
-function blacklistVilla() {
-  
+function blacklistVilla(xy) {
+  console.log("blacklist " + xy);
 }
 
 function doVillage() {
   var reportList = $("#report_table tr:not(:first-child)");
   if (reportList.length == 0) {
-    blacklistVilla();
+    blacklistVilla("todo");
     return;
   }
   var reportHtml = reportList[0].innerHTML;
@@ -49,11 +53,117 @@ function doVillage() {
   goto("screen=report&mode=all&view=" + reportId);
 }
 
+function scrapeReport() {
+  var def_xy = $("#attack_info_def")[0].innerHTML.match(/(\d+\|\d+)/)[1];
+  var building_levels = new Array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+  var building_data = JSON.parse($('#attack_spy_building_data').val());
+  for (i = 0; i < building_data.length; i++) {
+    var building = building_data[i];
+    building_levels[building_ids.indexOf(building.id)] = Number(building.level);
+  }
+  return {def_xy : def_xy, building_levels : building_levels}
+}
+
+function getCatIndex(building_levels) {
+  var cat_index = 0;
+  while (cat_index < building_levels.length && building_levels[cat_index] <= building_min[cat_index]) {
+    cat_index++;
+  }
+  return cat_index;
+}
+
+function setCatCookie(xy, buildings) {
+  setCookie("cat", xy + "," + buildings.join(","));
+}
+
+function getCatCookie() {
+  var cat_cookie = cookies.cat.split(",");
+  console.log(cat_cookie);
+  var xy = cat_cookie[0];
+  cat_cookie.shift();
+  return {xy : xy, building_levels : cat_cookie}
+}
+
+function doReport() {
+  var scraping = scrapeReport();
+  
+  var cat_index = getCatIndex(scraping.building_levels);
+  if (cat_index == scraping.building_levels.length) {
+    blacklistVilla(scraping.xy);
+    return;
+  }
+  
+  setCatCookie(scraping.def_xy, scraping.building_levels);
+  goto("screen=place");
+}
+
+function doPlace() {
+  var cat_cookie = getCatCookie();
+  var xy = cat_cookie.xy;
+  var building_levels = cat_cookie.building_levels;
+  
+  var cat_index = getCatIndex(building_levels);
+  if (cat_index == building_levels.length) {
+    blacklistVilla(xy);
+    return;
+  }
+  
+  console.log("Catting level " + building_levels[cat_index] + " " + building_ids[cat_index]);
+  
+  var num_cats = building_cats[building_levels[cat_index] - 1];
+  var max_cats = document.forms[0].catapult.outerHTML.match(/data-all-count="(\d+)"/)[1];
+  var spy = 0;
+  if (max_cats < 2*num_cats) {
+    spy = 1;
+  }
+  building_levels[cat_index]--;
+  if (getCatIndex(building_levels) == building_levels.length) {
+    spy = 1;
+  }
+  
+  if (spy > 0) document.forms[0].spy.value = spy;
+  document.forms[0].axe.value = num_axe;
+  document.forms[0].input.value = xy;
+  document.forms[0].catapult.value = num_cats;
+}
+
+function selectCatAndUpdateCookie() {
+  var cat_cookie = getCatCookie();
+  console.log(cat_cookie.building_levels);
+  var xy = cat_cookie.xy;
+  var building_levels = cat_cookie.building_levels;
+  
+  var cat_index = getCatIndex(building_levels);
+  if (cat_index == cat_cookie.length) {
+    console.log("Done catting");
+    return;
+  }
+  
+  var options = document.forms[0].building.options;
+  for (i = 0; i < options.length; i++) {
+    if (building_ids[cat_index] == options[i].value) {
+      document.forms[0].building.selectedIndex = i;
+      break;
+    }
+  }
+  building_levels[cat_index]--;
+  setCatCookie(xy, building_levels);
+}
+
 function main() {
   if (url.params.screen == "map") {
     doMap();
   } else if (url.params.screen == "info_village") {
     doVillage();
+  } else if (url.params.screen == "report") {
+    doReport();
+  } else if (url.params.screen == "place") {
+    if (url.params.try != "confirm") {
+      doPlace();
+    } else {
+      selectCatAndUpdateCookie();
+    }
   } else {
     goto("screen=map");
   }
